0e3db166c122f980487b5971a3d7a396c4f5b90f
Hassan Dawoud <45721246+hmdawoud@users.noreply.github.com>
2020-08-12 19:59:19 +0300

    If Statement

--- if-example.sh
+++ if-example.sh
@@ -9,7 +9,22 @@ else
 echo "no"
 fi
 
+echo "#############"
+
+# || = OR 
+# && = AND
+
 if [ $var -lt 0 ] || [ $var -eq 10 ]
+then
+echo "One or both conditions are true"
+else
+echo "two conditions are false"
+fi
+echo "#############"
+
+ # && = AND
+
+if [ $var -lt 0 ] && [ $var -eq 10 ]
 then
 echo "One or both conditions are true"
 else
